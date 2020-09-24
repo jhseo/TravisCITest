0eb09b00f263738a30b89e7117b7adbdc4938976
Junghun Seo <sjh7893@gmail.com>
2020-09-24 14:33:24 +0900

    feat: 프로젝트 추가

---
+++ TravisCITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravisCITest
+//
+//  Created by JunghunSeo on 2020/09/24.
+//  Copyright © 2020 JunghunSeo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
